aec14fd149c85322e669447ce6867f54e3200af1
Eng_Mohamed <Eng_Mohamed@DESKTOP-RF4V8A9>
2022-03-23 01:01:13 +0300

    list of prime

---
+++ KodluyoruzBootcamp/134/Prime_Listing/Prime_Listing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Prime_Listing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List all prime numbers from 1 to 10000
+
+            bool check = true;
+            int counter = 0;
+
+            for (int i = 2; i < 10000; i++)
+            {
+                for (int j  = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                       check = false;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    Console.WriteLine(i);
+                    counter++;
+                }
+                check = true;
+            }
+            Console.WriteLine("Count of Prime Number is "+ counter);
+        }
+    }
+}
