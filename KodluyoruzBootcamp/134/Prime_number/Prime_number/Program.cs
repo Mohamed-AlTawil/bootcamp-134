f87c2ace17024e952443616267a57edda1132d3a
Eng_Mohamed <Eng_Mohamed@DESKTOP-RF4V8A9>
2022-03-23 00:51:51 +0300

    Prime Number

---
+++ KodluyoruzBootcamp/134/Prime_number/Prime_number/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determine whether the number entered by the user is prime or not
+
+            Console.WriteLine("Enter Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            bool check = true;
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    check = false;  
+                    break;
+                }
+            }
+
+            if (check)
+            {
+                Console.WriteLine(num +": is a prime number ");
+            }
+            else
+            {
+                Console.WriteLine(num + ": is not a prime number ");
+            }
+
+        }
+    }
+}
