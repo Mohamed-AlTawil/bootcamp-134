f6098db9bdd73047ff81bce895489f23e3d7fd86
Eng_Mohamed <Eng_Mohamed@DESKTOP-RF4V8A9>
2022-03-24 18:22:37 +0300

    Circle

---
+++ KodluyoruzBootcamp/134/Area_Circle/Area_Circle/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+try
+{
+    String again = "";
+    do
+    {
+        Console.WriteLine(">> Enter The Radius Of The Circle (R)");
+        double p = Math.PI;
+        double R = Double.Parse(Console.ReadLine());
+        double area = p * Math.Pow(R, 2);
+        double circumference = 2 * p * R;
+        Console.WriteLine($">> The Area of Circle is {area} \n>> The Circumference of Circle is {circumference}");
+
+        Console.WriteLine("\n>> Do you want to enter another value? [Y/N]");
+        again = Console.ReadLine();
+
+    } while (again=="Y");
+
+}
+catch (Exception)
+{
+    Console.WriteLine(">> Enter just Number");
+}
